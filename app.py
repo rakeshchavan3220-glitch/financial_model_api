dc75a9f46a5d146bbe2b5062d2d2e484763bf451
rakeshchavan3220-glitch <rakeshchavan3220@gmail.com>
2026-08-27 12:02:18 +0530

    Add Flask App

---
+++ app.py
@@ -0,0 +1,15 @@
+open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return "AI Financial Performance Model API is running!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(features)
+    return jsonify({'performance_score': float(prediction[0])})
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
